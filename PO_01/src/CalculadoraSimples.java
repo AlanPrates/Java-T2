40089bdbb49fa6761b2d429f06a0b9f6db4056da
AlanPrates <alan_nz@live.com>
2023-11-16 16:43:29 -0300

    Adicionando comentarios

--- PO_01/src/CalculadoraSimples.java
+++ PO_01/src/CalculadoraSimples.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 public class CalculadoraSimples {
     public static void main(String[] args) {
+        // Cria um objeto Scanner para ler a entrada do teclado
         Scanner scanner = new Scanner(System.in);
 
+        // Solicita ao usuário que digite o primeiro número
         System.out.print("Digite o primeiro número: ");
         double num1 = scanner.nextDouble();
 
+        // Solicita ao usuário que digite o segundo número
         System.out.print("Digite o segundo número: ");
         double num2 = scanner.nextDouble();
 
+        // Solicita ao usuário que escolha a operação (+, -, *, /)
         System.out.print("Escolha a operação (+, -, *, /): ");
         char operacao = scanner.next().charAt(0);
 
         double resultado = 0;
 
+        // Realiza a operação escolhida com base nos números fornecidos
         switch (operacao) {
             case '+':
                 resultado = num1 + num2;
@@ -26,6 +31,7 @@ public class CalculadoraSimples {
                 resultado = num1 * num2;
                 break;
             case '/':
+                // Verifica se o segundo número não é zero antes de realizar a divisão
                 if (num2 != 0) {
                     resultado = num1 / num2;
                 } else {
@@ -38,8 +44,10 @@ public class CalculadoraSimples {
                 return;
         }
 
+        // Exibe o resultado da operação
         System.out.println("Resultado: " + resultado);
 
+        // Fecha o Scanner para evitar vazamento de recursos
         scanner.close();
     }
 }
--- PO_01/src/ConversorDeTemperatura.java
+++ PO_01/src/ConversorDeTemperatura.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class ConversorDeTemperatura {
     public static void main(String[] args) {
+        // Cria um objeto Scanner para ler a entrada do teclado
         Scanner scanner = new Scanner(System.in);
 
+        // Solicita ao usuário que digite a temperatura
         System.out.print("Digite a temperatura: ");
         double temperatura = scanner.nextDouble();
 
+        // Solicita ao usuário que escolha a unidade de temperatura (Celsius ou Fahrenheit)
         System.out.print("Escolha a unidade (C para Celsius, F para Fahrenheit): ");
         char unidade = scanner.next().charAt(0);
 
         double resultado = 0;
 
+        // Converte a temperatura com base na escolha do usuário
         if (unidade == 'C' || unidade == 'c') {
             resultado = celsiusParaFahrenheit(temperatura);
             System.out.println("Temperatura em Fahrenheit: " + resultado + " °F");
@@ -22,13 +26,16 @@ public class ConversorDeTemperatura {
             System.out.println("Unidade inválida.");
         }
 
+        // Fecha o Scanner para evitar vazamento de recursos
         scanner.close();
     }
 
+    // Converte de Celsius para Fahrenheit
     private static double celsiusParaFahrenheit(double celsius) {
         return (celsius * 9/5) + 32;
     }
 
+    // Converte de Fahrenheit para Celsius
     private static double fahrenheitParaCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5/9;
     }
