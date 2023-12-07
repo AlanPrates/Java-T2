d20d680d13f2ac9fbe7f34cfffeacc2ef8c770af
alanprates <alan_nz@live.com>
2023-12-07 16:51:23 -0300

    gi commit

--- TribunalJustica/src/Advogado.java
+++
@@ -1,5 +0,0 @@
-public class Advogado extends ParteEnvolvida {
-    public Advogado(String nome, String documento) {
-        super(nome, "Advogado", documento);
-    }
-}
--- TribunalJustica/src/Status.java
+++
@@ -1,6 +0,0 @@
-public enum Status {
-    ABERTO,
-    EM_DISTRIBUICAO,
-    JULGADO,
-    ARQUIVADO
-}
